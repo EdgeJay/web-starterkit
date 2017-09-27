0c31adad1e8b97b8cff6aea5aa3e1aee981bcd66
Wu Huijie <hjwu85@gmail.com>
2017-09-27 10:29:29 +0800

    utility class for generating HTML template at server-side

---
+++ src/client/utils/HTML.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import serialize from 'serialize-javascript';
+
+const HTML = ({ content, store }) => (
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Web Signup Boilerplate</title>
+      <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic" />
+      <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css" />
+      <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css" />
+    </head>
+    <body>
+      <div id="mount" dangerouslySetInnerHTML={{ __html: content }} />
+      <script dangerouslySetInnerHTML={{ __html: `window.__preload__ = ${serialize(store.getState())};` }} />
+      <script src="/assets/js/main.js"></script>
+    </body>
+  </html>
+);
+
+export default HTML;
