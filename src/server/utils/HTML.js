1ce642a5f02ced3538d5810dcb075168f7d4cbdb
Wu Huijie <hjwu85@gmail.com>
2017-09-30 13:38:24 +0800

    moved HTML template to server folder

--- src/server/utils/HTML.js
+++ src/server/utils/HTML.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/no-danger */
+
 import React from 'react';
+import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript';
 
 const HTML = ({ content, store }) => (
@@ -11,13 +14,37 @@ const HTML = ({ content, store }) => (
       <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic" />
       <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css" />
       <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css" />
+      <style>{`
+        html, body {
+          height: 100%;
+        }
+
+        #mount {
+          height: 100%;
+        }
+      `}</style>
     </head>
     <body>
       <div id="mount" dangerouslySetInnerHTML={{ __html: content }} />
       <script dangerouslySetInnerHTML={{ __html: `window.__preload__ = ${serialize(store.getState())};` }} />
-      <script src="/assets/js/app.js"></script>
+      <script src="/assets/js/app.js" />
     </body>
   </html>
 );
+
+HTML.defaultProps = {
+  content: '',
+};
+
+HTML.propTypes = {
+  content: PropTypes.string,
+  store: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+    PropTypes.object,
+  ]).isRequired,
+};
 
 export default HTML;
