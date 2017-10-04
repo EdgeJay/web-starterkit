49016213ac092b39d7d35c4861442b3cb627d2aa
Wu Huijie <hjwu85@gmail.com>
2017-10-04 17:34:47 +0800

    disable test csrf button when clicked

--- src/client/components/Features.js
+++ src/client/components/Features.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-danger */
+/* eslint-disable max-len */
 
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -7,17 +8,43 @@ import { bindActions, mapStateToProps } from '../stores';
 import * as actions from '../actions/main';
 import PageHeader from './PageHeader';
 
-const Features = props => (
-  <div>
-    <PageHeader>{'Features'}</PageHeader>
-    <h2>CSRF</h2>
-    <p dangerouslySetInnerHTML={{ __html: `Generated CSRF token is <code>${props.store.csrf}</code>. Tap on "Test CSRF token validity" to test the token.` }} />
-    <form action="/validate-csrf" method="POST">
-      <input type="hidden" name="_csrf" value={props.store.csrf} />
-      <button type="submit">Test CSRF token validity</button>
-    </form>
-  </div>
-);
+class Features extends React.PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      enableCSRFTest: true,
+    };
+  }
+
+  onTestCSRFToken(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      enableCSRFTest: false,
+    });
+  }
+
+  render() {
+    const buttonLabel = 'Test CSRF token validity';
+    const { enableCSRFTest } = this.state;
+
+    return (
+      <div>
+        <PageHeader>{'Features'}</PageHeader>
+        <h2>CSRF</h2>
+        <p dangerouslySetInnerHTML={{ __html: `Generated CSRF token is <code>${this.props.store.csrf}</code>. Tap on "${buttonLabel}" to test the token.` }} />
+        <form action="/validate-csrf" method="POST">
+          <input type="hidden" name="_csrf" value={this.props.store.csrf} />
+          <button
+            onClick={evt => this.onTestCSRFToken(evt)}
+            disabled={!enableCSRFTest}
+          >{buttonLabel}</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 Features.propTypes = {
   store: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
