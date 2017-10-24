d0bf2665256a820cb5d6e95853c648605b3b17e8
Wu Huijie <hjwu85@gmail.com>
2017-10-24 20:40:11 +0800

    test importing image

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PageHeader from './PageHeader';
+import staticAssets from '../utils/staticAssets';
 
 const Home = () => (
   <div>
     <PageHeader>{'Web Starter Kit'}</PageHeader>
+    <img src={staticAssets.images.smiley} alt="Smile!" height={200} />
   </div>
 );
 
