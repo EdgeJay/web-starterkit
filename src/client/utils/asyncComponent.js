8d1553a45bef5bf407c7eeda4f3476b47ba0b4eb
Wu Huijie <hjwu85@gmail.com>
2017-11-19 18:32:06 +0800

    remember to credit original code author

--- src/client/utils/asyncComponent.js
+++ src/client/utils/asyncComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Original source code taken from this gist: https://gist.github.com/acdlite/a68433004f9d6b4cbc83b5cc3990c194
 // getComponent is a function that returns a promise for a component
 // It will not be called until the first mount
 export default function asyncComponent(getComponent) {
