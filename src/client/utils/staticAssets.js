30d0573784b88975c87717dd18584764527014ec
Wu Huijie <hjwu85@gmail.com>
2017-12-01 15:55:42 +0800

    add favicons as part of staticAssets listing

--- src/client/utils/staticAssets.js
+++ src/client/utils/staticAssets.js
@@ -1,9 +1,21 @@
+import favicon from '../assets/img/favicons/favicon.ico';
+import favicon128 from '../assets/img/favicons/favicon-128.png';
+import favicon144 from '../assets/img/favicons/apple-touch-icon-144x144.png';
+import favicon152 from '../assets/img/favicons/apple-touch-icon-152x152.png';
+import favicon192 from '../assets/img/favicons/favicon-196x196.png';
+import favicon256 from '../assets/img/favicons/mstile-310x310.png';
 import smiley from '../assets/img/smiling.png';
 import rocket from '../assets/img/space/rocket.jpg';
 import indieFlower from '../assets/fonts/IndieFlower/IndieFlower.ttf';
 
 const staticAssets = {
   images: {
+    favicon,
+    favicon128,
+    favicon144,
+    favicon152,
+    favicon192,
+    favicon256,
     smiley,
     rocket,
   },
