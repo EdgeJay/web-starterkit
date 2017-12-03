9f2b56f637f34e70977426eab695402f5c03b362
Wu Huijie <hjwu85@gmail.com>
2017-12-03 16:44:34 +0800

    removed maximum-scale from viewport meta tag

--- src/server/utils/HTML.js
+++ src/server/utils/HTML.js
@@ -85,7 +85,7 @@ const HTML = ({ content, styles, store, asyncState }) => (
   <html lang="en">
     <head>
       <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black" />
