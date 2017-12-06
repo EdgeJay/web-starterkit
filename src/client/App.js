a611d0d815b8dc5a6725c937bced29ae97ec0f51
Wu Huijie <hjwu85@gmail.com>
2017-12-06 11:48:59 +0800

    moved RHL AppContainer to AppWrapper instead

--- src/client/App.js
+++ src/client/App.js
@@ -2,49 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-import { AppContainer } from 'react-hot-loader';
 
-const enableHMR = (process.env.ENABLE_WEBPACK_HMR === 'true' &&
-  process.env.NODE_ENV !== 'production');
-
-const App = (props) => {
-  if (enableHMR) {
-    return (
-      <AppContainer>
-        <Provider store={props.store}>
-          <Router history={props.history} routes={props.routes} />
-        </Provider>
-      </AppContainer>
-    );
-  }
-
-  return (
-    <Provider store={props.store}>
-      <Router history={props.history} routes={props.routes} />
-    </Provider>
-  );
-};
+const App = props => (
+  <Provider store={props.store}>
+    <Router history={props.history} routes={props.routes} />
+  </Provider>
+);
 
 App.propTypes = {
   store: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
     PropTypes.object,
   ]).isRequired,
   history: PropTypes.oneOfType([
     PropTypes.array,
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
     PropTypes.object,
   ]).isRequired,
   routes: PropTypes.oneOfType([
     PropTypes.array,
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
     PropTypes.object,
   ]).isRequired,
 };
--- src/client/AppWrapper.js
+++ src/client/AppWrapper.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { AppContainer } from 'react-hot-loader';
 import { AsyncComponentProvider } from 'react-async-component';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import { configureStore } from './stores';
 import renderRoutes from './utils/renderRoutes';
 import App from './App';
+
+const enableHMR = (process.env.ENABLE_WEBPACK_HMR === 'true' &&
+  process.env.NODE_ENV !== 'production');
 
 // eslint-disable-next-line dot-notation
 const store = configureStore(browserHistory, window['__preload__']);
@@ -18,7 +22,7 @@ const routes = renderRoutes();
 const render = () => {
   // wrap app with AsyncComponentProvider to rehydrate state
   // (refer to renderContent method in LandingController)
-  const WrappedApp = (
+  let WrappedApp = (
     <AsyncComponentProvider rehydrateState={asyncState}>
       <App
         store={store}
@@ -27,6 +31,12 @@ const render = () => {
       />
     </AsyncComponentProvider>
   );
+
+  if (enableHMR) {
+    WrappedApp = (
+      <AppContainer key={Math.random()}>{WrappedApp}</AppContainer>
+    );
+  }
 
   asyncBootstrapper(WrappedApp).then(() => {
     // render the app
