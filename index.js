24485c6672653ab747b3b70753b31cdcf99cb733
Wu Huijie <hjwu85@gmail.com>
2017-12-12 20:12:28 +0800

    moved babel config back into babelrc

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 require('dotenv').config();
-require('babel-register')({
-  presets: [
-    ['env', {
-      targets: {
-        node: 'current',
-      },
-    }],
-    'react',
-  ],
-  plugins: [
-    ['styled-components', {
-      ssr: true,
-    }],
-    'transform-react-jsx',
-    'transform-es2015-spread',
-    'transform-object-rest-spread',
-    ['transform-assets-import-to-string', {
-      extensions: ['.html', '.ico', '.png', '.jpg', '.jpeg', '.svg', '.gif', '.otf', '.ttf', '.json'],
-      baseDir: '/assets',
-      baseUri: '',
-    }],
-    'system-import-transformer',
-  ],
-});
+require('babel-register');
 
 // Load and initialize server app
 require('./src/server/app.js');
